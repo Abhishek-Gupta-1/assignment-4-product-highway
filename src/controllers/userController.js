3e2b195f0632857f1107daf8c134b5b6e957c75b
Abhishek-Gupta-1 <abhisheknew2023@gmail.com>
2024-11-12 14:42:16 +0530

    feat: signin user api working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,32 @@
-
-import { userRegister } from '../services/user.js';
+import { userRegister, userLogin } from "../services/user.js";
 
 export const register = async (req, res) => {
   try {
-    console.log("route hit",req.body);
+    console.log("route hit", req.body);
     const saveUser = await userRegister(req.body);
-console.log("save",saveUser);
+    console.log("save", saveUser);
     return res.status(201).json({
-        message: 'user registered successfully',
-        saveUser
+      message: "user registered successfully",
+      saveUser,
     });
-
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: 'Error registering user',
-      error: error.message
+      message: "Error registering user",
+      error: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const token = await userLogin(req.body);
+    return res.json({ token });
+  } catch (error) {
+    console.error("error: ", error);
+    return res.status(500).json({
+      message: error.message || "Internal Server error",
     });
   }
 };
--- src/services/user.js
+++ src/services/user.js
@@ -1,20 +1,21 @@
 import UserModal from "../modals/user.modal.js";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const userRegister = async (data) => {
   try {
-    const { username, password, bio } = data
-    console.log("data :",data)
+    const { username, password, bio } = data;
+    console.log("data :", data);
 
     // Check if username already exists
     const existingUser = await UserModal.findOne({ username });
-    console.log("user exist",existingUser)
+
     if (existingUser) {
-      return ({
+      console.log("user exist", existingUser);
+      return {
         success: false,
         message: "Username already exists",
-      });
+      };
     }
 
     // Hash password
@@ -30,15 +31,19 @@ export const userRegister = async (data) => {
 
     // Save user to database
     const savedUser = await newUser.save();
-    console.log("save in db",savedUser)
+    console.log("save in db", savedUser);
 
     // Generate JWT token
-    const token = jwt.sign({ userId: savedUser._id,username:savedUser.username }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
+    const token = jwt.sign(
+      { userId: savedUser._id, username: savedUser.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
 
     // Return success response without password
-    return ({
+    return {
       success: true,
       data: {
         user: {
@@ -52,12 +57,47 @@ export const userRegister = async (data) => {
         },
         token,
       },
-    });
+    };
   } catch (error) {
-    return({
+    return {
       success: false,
       message: "Error registering user",
       error: error.message,
-    });
+    };
+  }
+};
+
+export const userLogin = async (data) => {
+  try {
+    const { username, password } = data;
+    console.log("data :", data);
+
+    //check if user register or not
+    const existingUser = await UserModal.findOne({ username: username });
+    console.log(existingUser);
+    if (!existingUser) {
+      console.log("User didn't exist");
+      return "User didn't exist! Please Register first";
+    } else if (existingUser) {
+      const encryptPassword = existingUser.password;
+      const validPassword = await bcrypt.compare(password, encryptPassword);
+
+      if (!validPassword) {
+        return "Invalid credentials";
+      }
+
+      const token = jwt.sign(
+        { id: existingUser._id, username: existingUser.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "24h" }
+      );
+
+      console.log("token");
+
+      return token;
+    }
+  } catch (error) {
+    console.log("Error: ", error.message);
+    throw new Error("Failed to login User");
   }
 };
