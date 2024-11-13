db8846790fb46e75a1bde628d1ea08e26792ea3e
Abhishek-Gupta-1 <abhisheknew2023@gmail.com>
2024-11-13 17:29:56 +0530

    Update: minor update in user data update router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { authenticateUser } from "../middleware/auth.js";
-import { register, login, update} from "../controllers/userController.js";
+import { register, login, update } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/update", authenticateUser, update);
+router.put("/update", authenticateUser, update);
 
 export default router;
