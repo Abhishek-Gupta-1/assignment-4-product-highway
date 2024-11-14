a35419f490f6c728bb2bf7213c6f0e4973b86bb2
Abhishek-Gupta-1 <abhisheknew2023@gmail.com>
2024-11-15 00:13:30 +0530

    Refactor : sigin and register is working

--- src/routes/feed.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authenticateUser } from "../middleware/auth.js";
-import { getFeed } from "../controllers/feedController.js";
-
-const router = express.Router();
-
-router.get("/", authenticateUser, getFeed);
-
-export default router;
